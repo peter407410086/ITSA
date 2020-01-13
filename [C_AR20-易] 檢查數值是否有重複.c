4840bf207a345a25578459d5264ecc344d22a44a
peter407410086 <59797700+peter407410086@users.noreply.github.com>
2020-01-13 22:21:57 +0800

    Add files via upload

---
+++ [C_AR20-易] 檢查數值是否有重複.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[200], i, j, cnt, flag = 0;
+
+    scanf("%d", &cnt);
+
+    for(i = 0; i < cnt; i++){
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < cnt; i++){
+        for(j = 0; j < cnt; j++){
+            if(i == j)continue;
+            if(a[i] == a[j]){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1)break;
+    }
+
+    if(flag == 1)
+        printf("0\n");
+    else if(flag == 0)
+        printf("1\n");
+}
